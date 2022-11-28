46c4403f84e5e9f7c29363643f8d5cfc660cb281
alejox <alejoxdrums@gmail.com>
2022-11-27 19:59:18 -0500

    updates

--- src/router/index.js
+++ src/router/index.js
@@ -1,62 +1,61 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-
 const routes = [
   {
     path: '/',
     name: 'login',
     component: () => import('../views/auth/Index.vue'),
-  }, 
+  },
   {
     path: '/signIn',
     name: 'SignIn',
     component: () => import('../views/auth/SignIn.vue'),
-  }, 
+  },
   {
     path: '/signInCode',
     name: 'SignInCode',
     component: () => import('../views/auth/SignInCode.vue'),
-  }, 
+  },
   {
     path: '/register',
     name: 'Register',
     component: () => import('../views/auth/Register.vue'),
-  }, 
+  },
   {
     path: '/registerCreate',
     name: 'RegisterCreate',
     component: () => import('../views/auth/RegisterCreate.vue'),
-  }, 
+  },
   {
     path: '/registerPassword',
     name: 'RegisterPassword',
     component: () => import('../views/auth/RegisterPassword.vue'),
-  }, 
+  },
   {
     path: '/registerSuccess',
     name: 'RegisterSuccess',
     component: () => import('../views/auth/RegisterSuccess.vue'),
-  }, 
+  },
   {
     path: '/forgotPassword',
     name: 'ForgotPassword',
     component: () => import('../views/auth/ForgotPassword.vue'),
-  }, 
+  },
   {
     path: '/forgotCode',
     name: 'ForgotCode',
     component: () => import('../views/auth/ForgotCode.vue'),
-  }, 
+  },
   {
     path: '/resetPassword',
     name: 'ResetPassword',
     component: () => import('../views/auth/ResetPassword.vue'),
-  }, 
+  },
   {
     path: '/passwordSuccess',
     name: 'PasswordSuccess',
     component: () => import('../views/auth/PasswordSuccess.vue'),
-  }, 
+  },
   {
     path: '/home',
     name: 'Home',
@@ -81,53 +80,97 @@ const routes = [
     path: '/credit',
     name: 'Credit',
     component: () => import('../views/creditApply/Index.vue'),
-  }, 
+  },
   {
     path: '/creditApply',
     name: 'CreditApply',
     component: () => import('../views/creditApply/CreditApply.vue'),
-
-  }, 
+  },
   {
     path: '/creditSuccess',
     name: 'CreditSuccess',
     component: () => import('../views/creditApply/CreditSuccess.vue'),
-  }, 
+  },
   {
     path: '/simulateCredit',
     name: 'simulateCredit',
     component: () => import('../views/simulateCredit/Index.vue'),
-  }, 
+  },
   {
     path: '/simulateCreditApply',
     name: 'simulateCreditApply',
     component: () => import('../views/simulateCredit/SimulateCreditApply.vue'),
-  }, 
+  },
   {
     path: '/simulateSuccess',
     name: 'SimulateSuccess',
     component: () => import('../views/simulateCredit/SimulateSuccess.vue'),
-  }, 
+  },
   {
     path: '/profile',
     name: 'Profile',
     component: () => import('../views/profile/Index.vue'),
-  }, 
+  },
   {
     path: '/data',
     name: 'Data',
     component: () => import('../views/profile/Data.vue'),
-  }, 
+  },
   {
     path: '/dataWork',
     name: 'DataWork',
     component: () => import('../views/profile/DataWork.vue'),
-  }, 
+  },
   {
     path: '/dataSecurity',
     name: 'DataSecurity',
     component: () => import('../views/profile/DataSecurity.vue'),
-  }, 
+  },
+  {
+    path: '/autentication',
+    name: 'Autentication',
+    component: () => import('../views/autentication/Index.vue'),
+  },
+  {
+    path: '/autenticationCode',
+    name: 'AutenticationCode',
+    component: () => import('../views/autentication/AutenticationCode.vue'),
+  },
+  {
+    path: '/autenticationConfirmation',
+    name: 'AutenticationConfirmation',
+    component: () => import('../views/autentication/AutenticationConfirmation.vue'),
+  },
+  {
+    path: '/autenticationChangeNumber',
+    name: 'AutenticationChangeNumber',
+    component: () => import('../views/autentication/AutenticationChangeNumber.vue'),
+  },
+  {
+    path: '/autenticationNumber',
+    name: 'AutenticationNumber',
+    component: () => import('../views/autentication/AutenticationNumber.vue'),
+  },
+  {
+    path: '/autenticationNumberCode',
+    name: 'AutenticationNumberCode',
+    component: () => import('../views/autentication/AutenticationNumberCode.vue'),
+  },
+  {
+    path: '/autenticationNumberConfirmation',
+    name: 'AutenticationNumberConfirmation',
+    component: () => import('../views/autentication/AutenticationNumberConfirmation.vue'),
+  },
+  {
+    path: '/deactivate',
+    name: 'Deactivate',
+    component: () => import('../views/autentication/Deactivate.vue'),
+  },
+  {
+    path: '/deactivateConfirmation',
+    name: 'DeactivateConfirmation',
+    component: () => import('../views/autentication/DeactivateConfirmation.vue'),
+  },
 ];
 
 const router = createRouter({
